38a471a4f2c5e6f5d3ce2d3db8fc512b2e86403a
Nans Rey <nans2502@gmail.com>
2025-03-22 11:43:24 +0100

    Added comments

--- matled/src/app/states/weather.rs
+++ matled/src/app/states/weather.rs
@@ -5,14 +5,22 @@ use std::fs;
 
 const WEATHER_API_BASE_URL: &str = "https://api.open-meteo.com/v1/forecast";
 
+/* Public struct containing weather data */
 pub struct WeatherData {
-    weather_code:       String,
-    temp_max:           String,
-    temp_min:           String,
-    precipitation_sum:  String,
+    weather_code:       String,     // weather wmo codes: https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
+    temp_max:           String,     // maximum daily temperature
+    temp_min:           String,     // minimum daily temperature
+    precipitation_sum:  String,     // sum of daily precipitations (including rain, snow, hail)
 }
 
+
+// ================================================================= 
+//    Methods implementation
+// ================================================================= 
+
 impl WeatherData {
+
+    /* WeatherData constructor */
     pub fn new() -> Self {
         let data= get_weather();
         WeatherData {
@@ -23,10 +31,12 @@ impl WeatherData {
         }
     }
 
+    /* Method for drawing data to the screen */
     pub fn draw(&self) {
         println!("Drawing Weather");
     }
 
+    /* Method for fetching data from public API */
     pub fn fetch_data(&mut self) {
         let data= get_weather();
         self.weather_code =         code_to_weather(data.weather_code);
@@ -36,24 +46,17 @@ impl WeatherData {
     }
 }
 
-/* 
-    Static functions
-*/
-/* 
-Notes:
-    cargo add serde --features derive
-    cargo add serde_json
-    cargo add tokio --features full
-    cargo add reqwest --features json, blocking
-*/
+
+// ================================================================= 
+//    Static functions
+// ================================================================= 
+
+/* Function to get data from public weather API */
 fn get_weather() -> DailyData {
-    /*
-        URL part
-    */
 
-    // let correct_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Europe%2FBerlin&forecast_days=1";
+/* ______________________URL_PART______________________ */
 
-    // create a JSON with the request parameters
+    /* create a JSON with the request parameters */
     let params = json!({
         "latitude": "43.57",
         "longitude": "1.46",
@@ -68,31 +71,39 @@ fn get_weather() -> DailyData {
         "forecast_days": 1,
     });
 
+    /* Construct query params URL from the json */
     let params_url = json_to_query_string(&params);
+
+    /* Concatenate base URL and query params */
     let full_url = format!("{}?{}", WEATHER_API_BASE_URL, params_url);
+
     // println!("full_url: {full_url}");
 
-    /*
-        Request part
-    */
+/* ______________________REQUEST_PART______________________ */
 
+    /* Create Reqwest Client */
     let client = Client::new();
     let response = client
         .get(full_url)
         .header("User-Agent", "reqwest")
         .send();
+
     // println!("{response:?}");
 
-    // let body = response.unwrap().text().unwrap();
+    /* Deserialize response */
     let parsed: WeatherResponse = response
-        .unwrap()
-        .json::<WeatherResponse>()
-        .unwrap();
+    .unwrap()
+    .json::<WeatherResponse>()
+    .unwrap();
+
+    // let body = response.unwrap().text().unwrap();
     // println!("{parsed:?}");
 
     parsed.daily
 }
 
+
+/* Transforms json to query parameters URL */
 fn json_to_query_string(params_json: &serde_json::Value) -> String {
     params_json.as_object()
         .expect("Expected a json object")
@@ -110,7 +121,52 @@ fn json_to_query_string(params_json: &serde_json::Value) -> String {
         .join("&")
 }
 
-#[allow(dead_code)] // necessary to avoid warning because fields only used for deserialization
+
+/* Function to take out first element of Vec<> */
+fn first_element_or_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Default + Clone + Deserialize<'de>,
+    D: Deserializer<'de>,
+{
+    let vec: Vec<T> = Vec::deserialize(deserializer)?; // Deserialization as a Vec<T>
+    Ok(vec.into_iter().next().unwrap_or_default()) // Take out first element or send back Default
+}
+
+
+/* Function used to translate weather WMO code to weather String using json code file */
+fn code_to_weather(code: i32) -> String {
+    let mut description: String = "Invalid weather code".to_string();
+
+    /* Open json file as String */
+    let wmo_code_file =
+        fs::read_to_string("./src/app/states/res/wmo_codes.json")
+        .expect("Failed to read weather code JSON file");
+
+    /* Convert String as json Value */
+    let wmo_code_json: serde_json::Value =
+        serde_json::from_str(&wmo_code_file)
+        .expect("Invalid weather code JSON");
+
+    /* Get description field ("entry": "day": "description": "xx") */
+    if let Some(entry) = wmo_code_json.get(code.to_string()) {
+        description = entry
+            .get("day")
+            .and_then(|d| d.get("description"))
+            .unwrap().
+            to_string();
+    }
+
+    description
+}
+
+
+// ================================================================= 
+//    Static types
+// ================================================================= 
+
+/* The following types are used for deserialization */
+
+#[allow(dead_code)] // Necessary to avoid warning because fields only used for deserialization
 #[derive(Debug, Deserialize)]
 struct WeatherResponse {
     latitude:               f64,
@@ -124,7 +180,7 @@ struct WeatherResponse {
     daily:                  DailyData,
 }
 
-#[allow(dead_code)]
+#[allow(dead_code)] // Necessary to avoid warning because fields only used for deserialization
 #[derive(Debug, Deserialize)]
 struct DailyUnits {
     time:                       String,
@@ -134,10 +190,10 @@ struct DailyUnits {
     precipitation_sum:          String,
 }
 
-#[allow(dead_code)]
+#[allow(dead_code)] // Necessary to avoid warning because fields only used for deserialization
 #[derive(Debug, Deserialize)]
 struct DailyData {
-    #[serde(deserialize_with = "first_element_or_default")] // function used when deserializing, to take 1rst element of Vec<.>
+    #[serde(deserialize_with = "first_element_or_default")] // Function used to deserialize, to take out first element of Vec<.>
     time:                       String,
     #[serde(deserialize_with = "first_element_or_default")]
     weather_code:               i32,
@@ -147,33 +203,4 @@ struct DailyData {
     apparent_temperature_min:   f64,
     #[serde(deserialize_with = "first_element_or_default")]
     precipitation_sum:          f64,
-}
-
-fn first_element_or_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
-where
-    T: Default + Clone + Deserialize<'de>,
-    D: Deserializer<'de>,
-{
-    let vec: Vec<T> = Vec::deserialize(deserializer)?; // Désérialisation en Vec<T>
-    Ok(vec.into_iter().next().unwrap_or_default()) // Prend le premier élément ou renvoie le défaut
-}
-
-fn code_to_weather(code: i32) -> String {
-    let mut description: String = "Invalid weather code".to_string();
-
-    let wmo_code_file =
-        fs::read_to_string("./src/app/states/res/wmo_codes.json")
-        .expect("Failed to read weather code JSON file");
-    let wmo_code_json: serde_json::Value =
-        serde_json::from_str(&wmo_code_file)
-        .expect("Invalid weather code JSON");
-
-    if let Some(entry) = wmo_code_json.get(code.to_string()) {
-        description = entry
-            .get("day")
-            .and_then(|d| d.get("description"))
-            .unwrap().
-            to_string();
-    }
-    description
 }
--- matled/src/main.rs
+++ matled/src/main.rs
@@ -2,6 +2,13 @@
 mod app;
 use app::app_main;
 
+/* Notes:
+    cargo add serde --features derive
+    cargo add serde_json
+    cargo add tokio --features full
+    cargo add reqwest --features json, blocking
+*/
+
 fn main() {
     println!("Hello from main");
     app_main::app_main();
